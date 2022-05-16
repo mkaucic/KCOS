59c327a7a7236cacf89c40b58c5fea7dc85fd769
matija.kaucic <matakaucic@gmail.com>
2022-05-16 15:12:39 +0200

    Make about navigation items into buttons

--- app/assets/javascripts/application/about_switch.js
+++ app/assets/javascripts/application/about_switch.js
@@ -2,19 +2,25 @@ function showCenter ()  {
     document.getElementById('about__body--center').style.display = 'block';
     document.getElementById('about__body--team').style.display = 'none';
     document.getElementById('about__body--venues').style.display = 'none';
-    document.getElementById('about__navigation').style.color = 'white';
+    document.getElementById('button__center').style.color = 'white';
+    document.getElementById('button__team').style.color = 'white';
+    document.getElementById('button__venues').style.color = 'white';
 }
 
 function showTeam ()  {
     document.getElementById('about__body--center').style.display = 'none';
     document.getElementById('about__body--team').style.display = 'block';
     document.getElementById('about__body--venues').style.display = 'none';
-    document.getElementById('about__navigation').style.color = 'black';
+    document.getElementById('button__center').style.color = 'black';
+    document.getElementById('button__team').style.color = 'black';
+    document.getElementById('button__venues').style.color = 'black';
 }
 
 function showVenues ()  {
     document.getElementById('about__body--center').style.display = 'none';
     document.getElementById('about__body--team').style.display = 'none';
     document.getElementById('about__body--venues').style.display = 'block';
-    document.getElementById('about__navigation').style.color = 'black';
+    document.getElementById('button__center').style.color = 'black';
+    document.getElementById('button__team').style.color = 'black';
+    document.getElementById('button__venues').style.color = 'black';
 }
